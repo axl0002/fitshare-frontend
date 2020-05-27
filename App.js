75dc40c8ff2d795b864f9019e8a831760ec5fa80
Max Kovalovs <maxkovalovs@gmail.com>
2020-05-27 20:39:39 +0100

    Add mock user data to landing page

--- App.js
+++ App.js
@@ -7,19 +7,39 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-} from 'react-native';
+import { SafeAreaView } from 'react-native';
 
-import {
-  SearchBar,
-} from 'react-native-elements'
+import { SearchBar, ListItem } from 'react-native-elements';
+
+const users = [
+  {
+    name: 'jack',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+  {
+    name: 'jackson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+  {
+    name: 'jacklynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
+  },
+];
 
 const App: () => React$Node = () => {
   return (
-      <SafeAreaView>
-        <SearchBar placeholder="Type Here..." />
-      </SafeAreaView>
+    <SafeAreaView>
+      <SearchBar placeholder="Type Here..." />
+      {users.map((l, i) => (
+        <ListItem
+          key={i}
+          leftAvatar={{ source: { uri: l.avatar } }}
+          title={l.name}
+          subtitle={l.name}
+          bottomDivider
+        />
+      ))}
+    </SafeAreaView>
   );
 };
 
