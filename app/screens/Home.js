5f1ca0bbbceb1f0967648d2f2837df948383f25f
alex <axl18@ic.ac.uk>
2020-06-03 02:28:05 +0100

    Fetch user's friends list from DB and displays on home

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Swiper from 'react-native-swiper';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import Profile from './Profile';
 import Challenge from './Challenge';
 import { SearchBar, ListItem, Icon, Button} from 'react-native-elements';
@@ -27,9 +27,39 @@ const users = [
 ];
 
 export default class Home extends Component {
-  state = {
-    search: '',
+  _isMounted = false;
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: null,
+      search: '',
+      userid: '106063041617179551857',
+    };
   }
+
+  componentDidMount() {
+    this._isMounted = true;
+  	this.getFriends();
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  async getFriends() {
+      try {
+        let response = await fetch(
+          'https://fitshare-backend.herokuapp.com/friends/'.concat(this.state.userid)
+        );
+        let json = await response.json();
+        console.log(json);
+        // console.log(json[0]);
+        // console.log(json[1]);
+        this.setState({friends: json});
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
   updateSearch = search => {
     this.setState({ search });
@@ -56,15 +86,19 @@ export default class Home extends Component {
           onChangeText={this.updateSearch}
           value={search}
           />
-          {users.map((l, i) => (
-            <ListItem
-              key={i}
-              leftAvatar={{ source: { uri: l.avatar } }}
-              title={l.name}
-              subtitle={l.subtitle}
-              bottomDivider
-            />
-          ))}
+          <FlatList
+            ItemSeparatorComponent={this.FlatListItemSeparator}
+            data={this.state.friends}
+            renderItem={item => (
+              <TouchableOpacity>
+                <Image
+                  source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg' }}
+                  style={{ width: 40, height: 40, margin: 6 }}
+                />
+                <Text>  {item.item}  </Text>
+              </TouchableOpacity>
+            )}
+          />
         </View>
 
         <View style = {styles.container}>
