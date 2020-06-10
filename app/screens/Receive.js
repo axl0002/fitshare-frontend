dfa991c826e5394931f0e45f2874d7e0da61f9dd
alex <axl18@ic.ac.uk>
2020-06-10 22:58:34 +0100

    Add reply functionality

--- app/screens/Receive.js
+++ app/screens/Receive.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
 import { SearchBar, ListItem, Icon, Button, Input } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 import Video from 'react-native-video';
 
 import UserContext from '../context/UserContext';
 
 import styles from './../css/Styles';
 
-export default class Receive extends Component {
+class Receive extends Component {
 
   static contextType = UserContext;
   constructor(props) {
@@ -19,18 +20,56 @@ export default class Receive extends Component {
   }
 
   render() {
+
+    const { navigation } = this.props;
+    let button = (
+
+        <View style = {{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-start'}}>
+          <TouchableOpacity
+          onPress={() => navigation.navigate('Camera',
+          {
+            challenge: this.state.metadata,
+          })
+        }
+          >
+            <View>
+              <Icon
+                color='white'
+                className="material-icons"
+                name="radio-button-unchecked"
+                size={100}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+  );
     return (
-      <View>
-        <Text> { this.state.metadata } </Text>
-        <Video
-          source={{
-            uri: 'https://d10l22hqwt0sax.cloudfront.net/'.concat(this.state.s3key),
-          }}
-          style={styles.video}
-          controls={true}
-          resizeMode={'cover'}
-        />
+      <View style = {styles.container}>
+        <View>
+          <Text> { this.state.metadata } </Text>
+          <Video
+            source={{
+              uri: 'https://d10l22hqwt0sax.cloudfront.net/'.concat(this.state.s3key),
+            }}
+            style={styles.video}
+            controls={false}
+            resizeMode={'cover'}
+            repeat={true}
+            hideShutterView={true}
+          />
+        </View>
+        <View  style ={{position: 'absolute', left: 0, right: 0, bottom: 10}}>
+          <View style={{flexDirection: 'row', justifyContent: 'center' }}>
+            {button}
+          </View>
+        </View>
       </View>
     );
   }
+}
+
+export default function(props) {
+  const navigation = useNavigation();
+
+  return <Receive {...props} navigation={navigation} />;
 }
